210d625a428fe978db22351e74ef4b224bf8f732
= <=>
2017-09-29 21:17:50 +0200

    arabic validation

---
+++ resources/lang/ar/validation.php
@@ -0,0 +1,158 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Validation Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines contain the default error messages used by
+    | the validator class. Some of these rules have multiple versions such
+    | such as the size rules. Feel free to tweak each of these messages.
+    |
+    */
+
+    'accepted'             => 'يجب قبول حقل :attribute',
+    'active_url'           => 'حقل :attribute لا يُمثّل رابطًا صحيحًا',
+    'after'                => 'يجب على حقل :attribute أن يكون تاريخًا لاحقًا للتاريخ :date.',
+    'after_or_equal'       => 'The :attribute must be a date after or equal to :date.',
+    'alpha'                => 'يجب أن لا يحتوي حقل :attribute سوى على حروف',
+    'alpha_dash'           => 'يجب أن لا يحتوي حقل :attribute على حروف، أرقام ومطّات.',
+    'alpha_num'            => 'يجب أن يحتوي :attribute على حروفٍ وأرقامٍ فقط',
+    'array'                => 'يجب أن يكون حقل :attribute ًمصفوفة',
+    'before'               => 'يجب على حقل :attribute أن يكون تاريخًا سابقًا للتاريخ :date.',
+    'before_or_equal'      => 'The :attribute must be a date before or equal to :date.',
+    'between'              => [
+        'numeric' => 'يجب أن تكون قيمة :attribute بين :min و :max.',
+        'file'    => 'يجب أن يكون حجم الملف :attribute بين :min و :max كيلوبايت.',
+        'string'  => 'يجب أن يكون عدد حروف النّص :attribute بين :min و :max',
+        'array'   => 'يجب أن يحتوي :attribute على عدد من العناصر بين :min و :max',
+    ],
+    'boolean'              => 'يجب أن تكون قيمة حقل :attribute إما true أو false ',
+    'confirmed'            => 'حقل التأكيد غير مُطابق للحقل :attribute',
+    'date'                 => 'حقل :attribute ليس تاريخًا صحيحًا',
+    'date_format'          => 'لا يتوافق حقل :attribute مع الشكل :format.',
+    'different'            => 'يجب أن يكون الحقلان :attribute و :other مُختلفان',
+    'digits'               => 'يجب أن يحتوي حقل :attribute على :digits رقمًا/أرقام',
+    'digits_between'       => 'يجب أن يحتوي حقل :attribute بين :min و :max رقمًا/أرقام ',
+    'dimensions'           => 'الـ :attribute يحتوي على أبعاد صورة غير صالحة.',
+    'distinct'             => 'للحقل :attribute قيمة مُكرّرة.',
+    'email'                => 'يجب أن يكون :attribute عنوان بريد إلكتروني صحيح البُنية',
+    'exists'               => 'حقل :attribute لاغٍ',
+    'file'                 => 'الـ :attribute يجب أن يكون من ملفا.',
+    'filled'               => 'حقل :attribute إجباري',
+    'image'                => 'يجب أن يكون حقل :attribute صورةً',
+    'in'                   => 'حقل :attribute لاغٍ',
+    'in_array'             => 'حقل :attribute غير موجود في :other.',
+    'integer'              => 'يجب أن يكون حقل :attribute عددًا صحيحًا',
+    'ip'                   => 'يجب أن يكون حقل :attribute عنوان IP ذا بُنية صحيحة',
+    'json'                 => 'يجب أن يكون حقل :attribute نصا من نوع JSON.',
+    'max'                  => [
+        'numeric' => 'يجب أن تكون قيمة حقل :attribute مساوية أو أصغر لـ :max.',
+        'file'    => 'يجب أن لا يتجاوز حجم الملف :attribute :max كيلوبايت',
+        'string'  => 'يجب أن لا يتجاوز طول النّص :attribute :max حروفٍ/حرفًا',
+        'array'   => 'يجب أن لا يحتوي حقل :attribute على أكثر من :max عناصر/عنصر.',
+    ],
+    'mimes'                => 'يجب أن يكون حقل ملفًا من نوع : :values.',
+    'mimetypes'            => 'يجب أن يكون حقل ملفًا من نوع : :values.',
+    'min'                  => [
+        'numeric' => 'يجب أن تكون قيمة حقل :attribute مساوية أو أكبر لـ :min.',
+        'file'    => 'يجب أن يكون حجم الملف :attribute على الأقل :min كيلوبايت',
+        'string'  => 'يجب أن يكون طول نص :attribute على الأقل :min حروفٍ/حرفًا',
+        'array'   => 'يجب أن يحتوي حقل :attribute على الأقل على :min عُنصرًا/عناصر',
+    ],
+    'not_in'               => 'حقل :attribute لاغٍ',
+    'numeric'              => 'يجب على حقل :attribute أن يكون رقمًا',
+    'present'              => 'يجب تقديم حقل :attribute',
+    'regex'                => 'صيغة حقل :attribute .غير صحيحة',
+    'required'             => 'حقل :attribute مطلوب.',
+    'required_if'          => 'حقل :attribute مطلوب في حال ما إذا كان :other يساوي :value.',
+    'required_unless'      => 'حقل :attribute مطلوب في حال ما لم يكن :other يساوي :values.',
+    'required_with'        => 'حقل :attribute مطلوب عند ملىء حقل :values .',
+    'requiredwith'        => 'حقل :attribute مطلوب عند ملىء حقل :values .',
+    'required_with_all'    => 'حقل :attribute مطلوب إذا توفّر :values.',
+    'requiredwithout'     => 'حقل :attribute مطلوب إذا لم يتوفّر :values.',
+    'required_without'     => 'حقل :attribute مطلوب إذا لم يتوفّر :values.',
+    'required_without_all' => 'حقل :attribute مطلوب إذا لم يتوفّر :values.',
+    'same'                 => 'يجب أن يتطابق حقل :attribute مع :other',
+    'size'                 => [
+        'numeric' => 'يجب أن تكون قيمة حقل :attribute مساوية لـ :size',
+        'file'    => 'يجب أن يكون حجم الملف :attribute :size كيلوبايت',
+        'string'  => 'يجب أن يحتوي نص :attribute على :size حروفٍ/حرفًا بالظبط',
+        'array'   => 'يجب أن يحتوي حقل :attribute على :size عنصرٍ/عناصر بالظبط',
+    ],
+    'string'               => 'يجب أن يكون حقل :attribute نصآ.',
+    'timezone'             => 'يجب أن يكون :attribute نطاقًا زمنيًا صحيحًا',
+    'unique'               => 'قيمة حقل :attribute مُستخدمة من قبل',
+    'uploaded'             => 'فشل في تحميل الـ :attribute',
+    'url'                  => 'صيغة الرابط :attribute غير صحيحة',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Custom Validation Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify custom validation messages for attributes using the
+    | convention "attribute.rule" to name the lines. This makes it quick to
+    | specify a specific custom language line for a given attribute rule.
+    |
+    */
+
+    'custom'               => [
+        'attribute-name' => [
+            'rule-name' => 'custom-message',
+        ],
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Custom Validation Attributes
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used to swap attribute place-holders
+    | with something more reader friendly such as E-Mail Address instead
+    | of "email". This simply helps us make messages a little cleaner.
+    |
+    */
+
+    'attributes'           => [
+        'name'                  => 'الاسم',
+        'username'              => 'اسم المُستخدم',
+        'email'                 => 'البريد الالكتروني',
+        'first_name'            => 'الاسم',
+        'last_name'             => 'اسم العائلة',
+        'password'              => 'كلمة المرور',
+        'password_confirmation' => 'تأكيد كلمة المرور',
+        'city'                  => 'المدينة',
+        'country'               => 'الدولة',
+        'address'               => 'العنوان',
+        'phone'                 => 'الهاتف',
+        'mobile'                => 'الجوال',
+        'age'                   => 'العمر',
+        'sex'                   => 'الجنس',
+        'gender'                => 'النوع',
+        'day'                   => 'اليوم',
+        'month'                 => 'الشهر',
+        'year'                  => 'السنة',
+        'hour'                  => 'ساعة',
+        'minute'                => 'دقيقة',
+        'second'                => 'ثانية',
+        'title'                 => 'العنوان',
+        'content'               => 'المُحتوى',
+        'description'           => 'الوصف',
+        'excerpt'               => 'المُلخص',
+        'date'                  => 'التاريخ',
+        'time'                  => 'الوقت',
+        'available'             => 'مُتاح',
+        'size'                  => 'الحجم',
+        'photo'                 => 'الصورة',
+        'link'                  => 'رابط الملف',
+        'type'                  => 'النوع',
+        'location'              => 'محل الاقامة',
+        'is_client'             => 'الحاله',
+        'phone'                 => 'التليفون',
+        'idenity'               => 'الرقم القومى',
+    ],
+
+];
