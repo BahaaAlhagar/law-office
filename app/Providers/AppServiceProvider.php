24ece32c9c24d71fa6ce2cd334978f3591ce8302
Bahaa Alhagar <bahaa.rock@gmail.com>
2021-01-09 22:18:11 +0200

    removed start of week

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace App\Providers;
 
-use Carbon\Carbon;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Database\Eloquent\Relations\Relation;
